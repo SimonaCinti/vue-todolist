e7d2444d0b7b5fe1e8a70c57612fb5cbe8b71f23
Simona Cinti <simona.cinti2@gmail.com>
2020-11-10 15:36:15 +0100

    Todo list add todo

--- js/main.js
+++ js/main.js
@@ -4,11 +4,23 @@ const app = new Vue ({
     el: '#app',
 
     data: {
+        todo: [
+            'Studiare',
+            'Annaffiare le piante',
+            'Capire come fare a scriptare una todolist',
+        ],
 
+       userMessage:'',
 
     },
 
     methods: {
-
+        submit(){
+           if (this.userMessage === '') {this.userMessage =''} else {
+              this.todo.push(this.userMessage);
+            this.userMessage = '' 
+           } 
+           
+        }
     },    
 });
